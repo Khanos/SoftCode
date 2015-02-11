b7ea21ac6acaf54c49379c14281f3992ddf9316d
Khanos <KhanosTech@gmail.com>
2015-02-11 16:25:48 -0430

    Se sigue el tutorial hasta la seccion 5.10

--- tango_project/rango/about.py
+++ tango_project/rango/about.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse("Rango Says: Here is the about page. <a href='/rango/'> Index </a>")
+    return render_to_response('rango/about.html')
--- tango_project/tango_project/settings.py
+++ tango_project/tango_project/settings.py
@@ -13,6 +13,9 @@ TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 # Rutas para los archivos estaticos
 STATIC_PATH  = os.path.join(PROJECT_PATH, 'static')
 
+# Rutas para la base de datos
+DATABASE_PATH = os.path.join(PROJECT_PATH, 'rango.db')
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -24,13 +27,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'NAME': 'rango',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '5432'
     }
 }
 
@@ -63,12 +65,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media') # Absolute path to the media directory
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -137,7 +139,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'rango',
